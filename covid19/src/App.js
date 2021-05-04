20374a5853af6a183a566d5cd048c147d02f1b2b
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-05-04 13:25:17 -0700

    Country Droptdonlist implemented

--- covid19/src/App.js
+++ covid19/src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  // useEffect(() => {
+  //   const getCountriesData = async () => {
+  //     await fetch("https://disease.sh/v3/covid-19/countries")
+  //       .then((response) => {
+  //         response.json();
+  //       })
+  //       .then((data) => {
+  //         const countries = data.map((country) => ({
+  //           name: country.country,
+  //           value: country.countryInfo.iso2,
+  //         }));
+
+  //         setCountries(countries);
+  //       });
+  //   };
+  //   getCountriesData();
+  // }, []);
+  useEffect(() => {
+    const getCountriesData = async () => {
+      fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          // let sortedData = sortData(data);
+          setCountries(countries);
+          // setMapCountries(data);
+          // setTableData(sortedData);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
+  const onchangeCountry = (e) => {
+    setCountry(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header"></div>
+      <h1>Covid-19 Tracker</h1>
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value={country} onChange={onchangeCountry}>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          {countries.map((country) => (
+            <MenuItem value={country.value}>{country.name}</MenuItem>
+          ))}
+          {/* <MenuItem value="asad">asad</MenuItem>
+          <MenuItem value="asadq">asadq</MenuItem> */}
+        </Select>
+      </FormControl>
     </div>
   );
 }
